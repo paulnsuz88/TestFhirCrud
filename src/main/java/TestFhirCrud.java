52b341390a7d5329121712651d0ad933642b0718
paulnsuz88@gmail.com <paulnsuz88@gmail.com>
2025-03-24 18:38:46 -0600

    Initial commit

---
+++ src/main/java/TestFhirCrud.java
@@ -0,0 +1,27 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+
+public class TestFhirCrud {
+        public static void main(String[] args) throws IOException {
+
+            OkHttpClient client = new OkHttpClient();
+            final String baseFhirServerUrl = "http://hapi.fhir.org/baseR4";
+            final int patientID = 596742;
+            Request request = new Request.Builder()
+                    .url(baseFhirServerUrl + "/Patient/" + patientID)
+                    .build();
+
+            try (Response response = client.newCall(request).execute()) {
+                if (response.body() != null) {
+                    System.out.println(response.body().string());
+                }
+                else {
+                    System.err.println("Empty Response!");
+                }
+            }
+        }
+    }
