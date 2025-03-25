f73098eb6dfd3e21599c4844800db5db69fdadbc
paulnsuz88@gmail.com <paulnsuz88@gmail.com>
2025-03-24 19:35:57 -0600

    added code to extract two values from response JSON

--- src/main/java/TestFhirCrud.java
+++ src/main/java/TestFhirCrud.java
@@ -1,6 +1,8 @@
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
@@ -15,12 +17,22 @@ public class TestFhirCrud {
                     .url(baseFhirServerUrl + "/Patient/" + patientID)
                     .build();
 
-            System.out.print("Retrieving Patient resource with ID=" + patientID + "...");
+            System.out.print("Retrieving Patient resource with ID=" + patientID + " ...");
             try (Response response = client.newCall(request).execute()) {
                 System.out.println("\nHTTP Status return code: " + response.code() + " (" + response.message() + ")");
-                if (response.body() != null) {
-                    System.out.println("Resource body returned:");
-                    System.out.println(response.body().string());
+                var responseBody = response.body();
+                if (responseBody != null) {
+                    String responseBodyStr = responseBody.string();
+                    System.out.println("Response body returned:");
+                    System.out.println(responseBodyStr);
+                    // parse id value from response JSON, using Jackson
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    JsonNode rootNode = objectMapper.readTree(responseBodyStr);
+                    String resourceType = rootNode.get("resourceType").toString();
+                    int resourceId = rootNode.get("id").asInt();
+                    System.out.println("Extracted values:");
+                    System.out.println("resourceType = " + resourceType);
+                    System.out.println("id = " + resourceId);
                 }
             }
         }
